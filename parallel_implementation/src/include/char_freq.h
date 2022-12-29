54a869b83c66f9d03b7182fefc3c2011ee890985
Cristiano <andrea.cristiano@hpc-head-n2.unitn.it>
2022-12-29 21:40:08 +0100

    bugfix wrong evaluation of the frequency of the added element

--- parallel_implementation/src/include/char_freq.h
+++ parallel_implementation/src/include/char_freq.h
@@ -5,8 +5,8 @@
 #include "../utils/print_utils.h"
 
 enum Position {
-    FIRST = 1,
-    LAST
+    FIRST = 0,
+    LAST,
 };
 
 typedef struct CharFreq {
@@ -21,6 +21,6 @@ typedef struct CharFreqDictionary {
 
 extern void getCharFreqsFromText(CharFreqDictionary *dict, char text[], long len, int pid);
 extern void sortCharFreqs(CharFreqDictionary *res);
-extern void appendToCharFreqs(CharFreqDictionary *dict, char character, int pos);
-extern void mergeCharFreqs(CharFreqDictionary *dict, CharFreq *charFreqs, int size); 
+extern void appendToCharFreqs(CharFreqDictionary *dict, CharFreq *charFreq, int pos);
+extern void mergeCharFreqs(CharFreqDictionary *dict, CharFreq *charFreqs, int size);
 extern void printCharFreqs(CharFreqDictionary *dict);
